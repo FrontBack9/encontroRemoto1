406aa78cb196f956d98a33c6fbe2633f413a794d
Aluno de Front End Senai <AlunoDeFrontEndSenai@gmail.com>
2022-05-15 17:36:49 -0300

    inicializa o projeto

---
+++ Resolucao.js
@@ -0,0 +1,20 @@
+
+let listaPecas = 30;
+let peso = 90;
+let nomePeca = "Barra de direção";
+
+
+
+if (peso>=100){
+    console.log ("Peca tem peso suficiente para cadastrar");
+} else{ 
+    console.log("Peca muito leve");
+
+}
+if(listaPecas>= 10) {
+    console.log("A lista está cheia nao tem capacidade suficiente para cadastrar");
+}
+if(nomePeca.length < 3){
+    console.log("Nome muito curto");
+}
+console.log("fim do programa");
